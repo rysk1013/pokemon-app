a31f6105f7f7ab7c4f067f973a21ffcc37ce9ebb
rysk1013 <0829ryosuke@gmail.com>
2023-10-22 23:13:23 +0900

    create loading view

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { getAllPokemon } from "./utils/pokemon";
 import "./App.css";
 
@@ -6,17 +6,29 @@ function App() {
   // エンドポイント
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
 
-  // 全てのポケモンデータを取得
-  const fetchPokemonData = async () => {
-    return await getAllPokemon(initialURL);
-  };
+  const [loading, setLoading] = useState(true);
 
   // 初期画面
   useEffect(() => {
+    // 全てのポケモンデータを取得
+    const fetchPokemonData = async () => {
+      let res = await getAllPokemon(initialURL);
+      console.log(res);
+      setLoading(false);
+    };
+
     fetchPokemonData();
   }, []);
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {loading ? (
+        <h1>ロード中・・・</h1>
+      ) : (
+        <h1>ポケモンデータを取得しました</h1>
+      )}
+    </div>
+  );
 }
 
 export default App;
