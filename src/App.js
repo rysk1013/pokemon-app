401a01a0fd47b5aa206963ba9160f4e851088335
rysk1013 <0829ryosuke@gmail.com>
2023-10-22 18:19:51 +0900

    create getAllPokemonData

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import React, { useEffect } from "react";
+import { getAllPokemon } from "./utils/pokemon";
 import "./App.css";
 
 function App() {
+  // エンドポイント
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
+
+  // 全てのポケモンデータを取得
+  const fetchPokemonData = async () => {
+    return await getAllPokemon(initialURL);
+  };
+
+  // 初期画面
+  useEffect(() => {
+    fetchPokemonData();
+  }, []);
+
   return <div className="App"></div>;
 }
 
