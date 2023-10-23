52d1b1fd5329da1a8fadb42182eedf7b222f2fc9
rysk1013 <0829ryosuke@gmail.com>
2023-10-23 15:42:47 +0900

    create card component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { getAllPokemon, getPokemon } from "./utils/pokemon";
 import "./App.css";
+import Card from "./components/Card/Card";
 
 function App() {
   // エンドポイント
@@ -39,7 +40,11 @@ function App() {
       {loading ? (
         <h1>ロード中・・・</h1>
       ) : (
-        <h1>ポケモンデータを取得しました</h1>
+        <div className="pokemonCardContainer">
+          {pokemonData.map((pokemon, i) => {
+            return <Card key={i} pokemon={pokemon} />;
+          })}
+        </div>
       )}
     </div>
   );
