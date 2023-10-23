d1ae1dd1fcae26ed86c20350c641a2497f4d27e0
rysk1013 <0829ryosuke@gmail.com>
2023-10-23 13:58:34 +0900

    get pokemon details

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { getAllPokemon } from "./utils/pokemon";
+import { getAllPokemon, getPokemon } from "./utils/pokemon";
 import "./App.css";
 
 function App() {
@@ -7,18 +7,32 @@ function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
 
   const [loading, setLoading] = useState(true);
+  const [pokemonData, setPokemonData] = useState([]);
 
   // 初期画面
   useEffect(() => {
-    // 全てのポケモンデータを取得
     const fetchPokemonData = async () => {
+      // 全てのポケモンデータを取得
       let res = await getAllPokemon(initialURL);
-      console.log(res);
+      // 各ポケモンの詳細データを取得
+      loadPokemon(res.results);
       setLoading(false);
+    };
+
+    const loadPokemon = async (data) => {
+      let _pokemonData = await Promise.all(
+        data.map((pokemon) => {
+          let pokemonRecord = getPokemon(pokemon.url);
+          return pokemonRecord;
+        })
+      );
+      setPokemonData(_pokemonData);
     };
 
     fetchPokemonData();
   }, []);
+
+  console.log(pokemonData);
 
   return (
     <div className="App">
